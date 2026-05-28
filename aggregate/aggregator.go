88d60a407149262657d077538eb5fc1067370140
Pedro <psotsan@pm.me>
2026-05-28 10:02:37 +0200

    Process now accepts Writer both for output and errors. TestProcess implemented

--- aggregate/aggregator.go
+++ aggregate/aggregator.go
@@ -4,23 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-const separator = ","
+const (
+	separator = ","
+	lineLimit = 100_000
+)
 
 func splitValidateLine(line string) ([]string, error) {
 	var err error
 	s := strings.Split(line, separator)
 
 	if len(s) != 2 {
-		err = fmt.Errorf("expected length 2. Got length %d", len(line))
+		err = fmt.Errorf("expected length 2. Got length %d", len(s))
 		return nil, err
 	}
 
 	s[0] = strings.Trim(s[0], " ")
+	s[0] = strings.Trim(s[0], "\t")
 	s[1] = strings.Trim(s[1], " ")
+	s[1] = strings.Trim(s[1], "\t")
 
 	if s[0] == "" || s[1] == "" {
 		err = fmt.Errorf("empty field(s) found")
@@ -28,6 +34,16 @@ func splitValidateLine(line string) ([]string, error) {
 	}
 
 	return s, nil
+}
+
+func keysToStrSlice(m map[string]float64) []string {
+	var s []string
+
+	for key := range m {
+		s = append(s, key)
+	}
+
+	return s
 }
 
 func AggregateLines(lines []string, errs *[]error) map[string]float64 {
@@ -61,8 +77,7 @@ func AggregateLines(lines []string, errs *[]error) map[string]float64 {
 	return agg
 }
 
-func Process(r io.Reader) (map[string]float64, []error) {
-	const lineLimit = 100_000
+func Process(r io.Reader, w io.Writer, errW io.Writer) (map[string]float64, []error) {
 	var batch []string
 	errs := make([]error, 0)
 	scanner := bufio.NewScanner(r)
@@ -73,11 +88,24 @@ func Process(r io.Reader) (map[string]float64, []error) {
 		if n > lineLimit {
 			err := fmt.Errorf("Process: r exceeds maximum line limit")
 			errs = append(errs, err)
+			fmt.Fprintln(errW, err)
 			return nil, errs
 		}
 		batch = append(batch, scanner.Text())
 	}
 
 	ret := AggregateLines(batch, &errs)
+
+	for _, err := range errs {
+		fmt.Fprintln(errW, err)
+	}
+
+	sl := keysToStrSlice(ret)
+	sort.Strings(sl)
+
+	for _, s := range sl {
+		fmt.Fprintf(w, "%s: %.1f\n", s, ret[s])
+	}
+
 	return ret, errs
 }
--- aggregate/aggregator_test.go
+++ aggregate/aggregator_test.go
@@ -2,6 +2,7 @@ package aggregate
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -114,11 +115,110 @@ func TestAggregateLines(t *testing.T) {
 				t.Fatalf("AggregateLines(%q): expected %d errors, got %d errors", tt.name, tt.errors, gotErr)
 			}
 
+			if len(got) != len(tt.want) {
+				t.Fatalf("AggregateLines(%q): got and want lengths differ\nlen(got): %d\nlen(want): %d",
+					tt.name, len(got), len(tt.want))
+			}
+
 			for key, val := range got {
 				if tt.want[key] != val {
 					t.Errorf("AggregateLines(%q): expected %s -> %.1f, got %s -> %.1f",
 						tt.name, key, tt.want[key], key, val)
 				}
+			}
+		})
+	}
+}
+
+func TestProcess(t *testing.T) {
+	var r1 *strings.Reader = strings.NewReader(`sales, 10
+	purchases, 19
+	sales, 3
+	purchases, 11
+	sales, 2`)
+
+	var r2 *strings.Reader = strings.NewReader(`purchases, 20
+	sales, twenty
+	sales, 10
+	sales, 15, 80
+	purchases, ^23`)
+
+	var r3 *strings.Reader = strings.NewReader(strings.Repeat("sales, 10\n", lineLimit+1))
+
+	ps := []struct {
+		name    string
+		r       *strings.Reader
+		want    string
+		wantMap map[string]float64
+		errors  int
+	}{
+		{
+			name: "5 valid lines with 2 keys",
+			r:    r1,
+			want: "purchases: 30.0\nsales: 15.0\n",
+			wantMap: map[string]float64{
+				"sales":     15,
+				"purchases": 30,
+			},
+		},
+		{
+			name: "3 invalid  lines and 2 valid lines",
+			r:    r2,
+			want: "purchases: 20.0\nsales: 10.0\n",
+			wantMap: map[string]float64{
+				"purchases": 20,
+				"sales":     10,
+			},
+			errors: 3,
+		},
+		{
+			name:    "limit exceeded",
+			r:       r3,
+			wantMap: nil,
+			errors:  1,
+		},
+	}
+
+	for _, tt := range ps {
+		t.Run(tt.name, func(t *testing.T) {
+			var sb, errSb strings.Builder
+			var gotErrW int
+			got, errs := Process(tt.r, &sb, &errSb)
+			gotErr := len(errs)
+			errW := strings.Trim(errSb.String(), "\n")
+
+			if errSb.Len() == 0 {
+				gotErrW = 0
+			} else {
+				gotErrW = len(strings.Split(errW, "\n"))
+			}
+
+			if gotErr != tt.errors {
+				for _, err := range errs {
+					fmt.Println(err)
+				}
+				t.Fatalf("Process(%q): expected %d returned errors, got %d errors", tt.name, tt.errors, gotErr)
+			}
+
+			if gotErrW != tt.errors {
+				fmt.Printf("%v", errW)
+				t.Fatalf("Process(%q): expected %d errors in Writer, got %d errors", tt.name, tt.errors, gotErrW)
+			}
+
+			if len(got) != len(tt.wantMap) {
+				t.Fatalf("Process(%q): got and want lengths differ\nlen(got): %d\nlen(want): %d",
+					tt.name, len(got), len(tt.wantMap))
+			}
+
+			for key, val := range got {
+				if tt.wantMap[key] != val {
+					t.Errorf("Process(%q): expected %s -> %.1f, got %s -> %.1f",
+						tt.name, key, tt.wantMap[key], key, val)
+				}
+			}
+
+			if sb.String() != tt.want {
+				t.Errorf("Process(%q): expected %s\ngot %s", tt.name, tt.want, sb.String())
 			}
 		})
 	}
